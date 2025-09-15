1bc2eb0830eb9ed60a0240307da9f3f073eb60fa
Heytt Satra <heyttsatra17@gmail.com>
2025-09-16 01:02:44 +0530

    added analytic

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Experience from "./components/Experience/Experience";
 import Work from "./components/Work/Work";
 import Education from "./components/Education/Education";
 import Footer from "./components/Footer/Footer";
-import BlurBlob from './components/BlurBlob';
+import BlurBlob from "./components/BlurBlob";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <div className="bg-gradient-to-b from-black to-[#ff0000]">
-      <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+      <BlurBlob position={{ top: "35%", left: "20%" }} size={{ width: "30%", height: "40%" }} />
       <div className="absolute inset-0 bg-[conic-gradient(#800000_90deg,transparent_90deg_180deg,#800000_180deg_270deg,transparent_270deg)] bg-[size:28px_48px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <div className="relative pt-20">
         <Navbar />
@@ -20,8 +21,8 @@ const App = () => {
         <Experience />
         <Work />
         <Education />
-        
         <Footer />
+        <Analytics />
       </div>
     </div>
   );
