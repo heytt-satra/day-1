f45dcf710bb88a8879f19f68b4aa8328a26177b9
Heytt Satra <heyttsatra17@gmail.com>
2025-09-20 09:03:35 +0530

    speed insigths added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Education from "./components/Education/Education";
 import Footer from "./components/Footer/Footer";
 import BlurBlob from "./components/BlurBlob";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Education />
         <Footer />
         <Analytics />
+        <SpeedInsights/>
       </div>
     </div>
   );
